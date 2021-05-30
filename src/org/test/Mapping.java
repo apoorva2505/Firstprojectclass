c02cf41908e2b4a9326c64d40a7c263f46cff137
apoorva2505 <apoorvadevaraj73@gmail.com>
2021-05-30 07:22:10 +0530

    push

---
+++ src/org/test/Mapping.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapping {
+	public static void main(String[] args) {
+		
+	
+ Map<Integer,String> M=new HashMap<>();
+ M.put(1, "AAA");
+ System.out.println(M);
+ 
+	}
+}
